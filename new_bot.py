600801c907376bcac44fa95b894090332b62d159
Ryan Lieu <ryan@pace.group>
2022-02-10 11:47:45 -0500

    playing with settings + added more help info

--- new_bot.py
+++ new_bot.py
@@ -1,6 +1,7 @@
 # new_bot.py
 import os
 import discord
+from discord.ext import commands
 import generate
 
 from discord.commands import Option
@@ -10,18 +11,22 @@ from types import SimpleNamespace
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = discord.Bot()
+bot = commands.Bot(
+    command_prefix="/",
+    description="FWB VQGAN BOT",
+    intents=discord.Intents.default()
+)
 
-@bot.slash_command(guild_ids=['930209526362284042'])
+@bot.slash_command(name="generate_image", description="Creates a VQGAN+CLIP generated image, see options for details", guild_ids=['930209526362284042'])
 async def generate_image(
         ctx: discord.ApplicationContext,
         prompts: Option(str, "Text prompts ex. (apple | surreal:0.5)", default=""),
-        image_prompts: Option(str, "Image prompts/target image URL ex. (apple | surreal:0.5)", default=""),
-        max_iterations: Option(int, "Number of iterations", default=500),
+        image_prompts: Option(str, "Image prompts/target image URL ex. (https://fwb.com/image.png)", default=""),
+        max_iterations: Option(int, "Number of iterations", default=300),
         display_freq: Option(int, "Save image iterations", default=50),
         width: Option(int, "Image width", default=256),
         height: Option(int, "Image height", default=256),
-        init_image: Option(str, "Initial image URL", default=""),
+        init_image: Option(str, "Initial image URL ex. (https://fwb.com/image.png)", default=""),
         init_noise: Option(str, "Initial noise image", choices=["pixels", "gradient"], default=""),
         init_weight: Option(str, "Initial weight (float)", default="0"),
         clip_model: Option(str, "CLIP model (e.g. ViT-B/32, ViT-B/16)", choices=["ViT-B/32", "ViT-B/16"], default="ViT-B/32"),
@@ -74,55 +79,12 @@ async def generate_image(
     setattr(final_args, 'video_style_dir', None)
     setattr(final_args, 'cuda_device', 'cuda:0')
 
-    # final_args['prompts'] = prompts
-    # final_args['image_prompts'] = image_prompts
-    # final_args['max_iterations'] = max_iterations
-    # final_args['display_freq'] = display_freq
-    # final_args['size'] = [width, height]
-    # final_args['init_image'] = init_image
-    # final_args['init_noise'] = init_noise
-    # final_args['init_weight'] = float(init_weight)
-    # final_args['clip_model'] = clip_model
-    # final_args['vqgan_config'] = 'checkpoints/vqgan_imagenet_f16_16384.yaml'
-    # final_args['vqgan_checkpoint'] = 'checkpoints/vqgan_imagenet_f16_16384.ckpt'
-    # final_args['noise_prompt_seeds'] = [] if not noise_prompt_seeds else [int(x.strip()) for x in noise_prompt_seeds.split("|")]
-    # final_args['noise_prompt_weights'] = [] if not noise_prompt_weights else [float(x.strip()) for x in noise_prompt_weights.split("|")]
-    # final_args['step_size'] = float(step_size)
-    # final_args['cut_method'] = cut_method
-    # final_args['cutn'] = cutn
-    # final_args['cut_pow'] = float(cut_pow)
-    # final_args['seed'] = None if seed == -1 else seed
-    # final_args['optimiser'] = optimiser
-    # final_args['output'] = output
-
-    # final_args['make_video'] = False
-    # final_args['make_zoom_video'] = False
-    # final_args['zoom_start'] = 0
-    # final_args['zoom_frequency'] = 10
-    # final_args['zoom_scale'] = 0.99
-    # final_args['zoom_shift_x'] = 0
-    # final_args['zoom_shift_y'] = 0
-    # final_args['prompt_frequency'] = 0
-    # final_args['video_length'] = 10
-    # final_args['output_video_fps'] = 0
-    # final_args['input_video_fps'] = 15
-    # final_args['cudnn_determinism'] = False
-    # final_args['augments'] = [],
-    # final_args['video_style_dir'] = None
-    # final_args['cuda_device'] = 'cuda:0'
-
-    
-
-
-
-    # print(final_args)
     generate.generate_image(final_args)
-
-
     with open(output, "rb") as fh:
-        f = discord.File(fh, filename="output.png")
+        f = discord.File(fh, filename=output)
     await ctx.respond(file=f)
-    await ctx.respond(final_args)
+
+    # await ctx.respond(final_args)
 
 print("VQGAN discord bot up and running!")
 bot.run(TOKEN)
